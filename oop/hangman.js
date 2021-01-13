4bb25eb1a1206f6f05792e59f58cfb72879ece6b
Dan Buckland <danbucklan@gmail.com>
2021-01-13 13:53:41 +0000

    Add Hangman example using prototypal inheritance

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,10 +1,61 @@
+// Setup the word instance as an array of lowercase letters
+// Setup another instance property to store guessed letters
+// Create a method that gives you the word puzzle back with guesses e.g. "st*a*y"
+
 const Hangman = function(word, guesses) {
-  this.word = word
-  this.guesses = guesses
+  this.word = word.toLowerCase().split('')
+  this.guessedLetters = ['a', 'e', 'i', 'o', 'u']
+  this.remainingGuesses = guesses
 }
 
-const game1 = new Hangman('california', 5)
-const game2 = new Hangman('steady', 4)
+Hangman.prototype.getPuzzle = function() {
+  let puzzle = ''
+  this.word.forEach(char => {
+    if (this.guessedLetters.includes(char) || char === ' ') {
+      puzzle += char
+    } else {
+      puzzle += '*'
+    }
+  })
+  return puzzle
+}
 
-console.log(game1)
-console.log(game2)
+Hangman.prototype.guess = function(guess) {
+  if (!this.remainingGuesses) {
+    console.log('You are all out of guesses!')
+    return
+  }
+
+  if (guess.length > 1 || guess.length === ' ' || !guess.length) return
+  
+  guess = guess.toLowerCase()
+  if (this.guessedLetters.includes(guess)) {
+    console.log(`You already guessed ${guess}!`)
+    return
+  }
+  this.remainingGuesses--
+  
+  let beforeState = this.getPuzzle()
+  this.guessedLetters.push(guess)
+  if (this.getPuzzle() === beforeState) {
+    console.log(`There are no ${guess}'s in the word. Guesses remaining: ${this.remainingGuesses}`)
+  } else {
+    console.log(`You found a match. Guesses remaining: ${this.remainingGuesses}`)
+  }
+}
+
+const game1 = new Hangman('California', 5)
+const game2 = new Hangman('steady', 4)
+const game3 = new Hangman('High Score', 10)
+
+console.log(game1.getPuzzle())
+console.log(game2.getPuzzle())
+console.log(game3.getPuzzle())
+game1.guess('c')
+game1.guess('i')
+game1.guess('f')
+game1.guess('x')
+game1.guess('x')
+game1.guess('a')
+game1.guess('b')
+console.log(game1.getPuzzle())
--- oop/person.js
+++ oop/person.js
@@ -1,11 +1,30 @@
-const Person = function(firstName, lastName, age) {
+// Prototypal Inheritance
+const Person = function(firstName, lastName, age, likes = []) {
   this.firstName = firstName
   this.lastName = lastName
   this.age = age
+  this.likes = likes
 }
 
-const me = new Person('Dan', 'Buckland', 33)
-const person2 = new Person('Milly', 'Turner', 12)
+Person.prototype.getBio = function () {
+  let bio = `${this.firstName} is ${this.age}.`
 
-console.log(me)
+  this.likes.forEach((like) => {
+    bio += ` ${this.firstName} likes ${like}.`
+  })
+  return bio
+}
+
+Person.prototype.setName = function (fullName) {
+  const names = fullName.split(' ')
+  this.firstName = names[0]
+  this.lastName = names[1]
+}
+
+const me = new Person('Dan', 'Buckland', 33, ['Xbox', 'Halo', 'VR'])
+const person2 = new Person('Milly', 'Turner', 12, ['horses', 'school', 'drag'])
+
+console.log(me.getBio())
+console.log(person2.getBio())
+person2.setName('Alexis Matteo')
 console.log(person2)
