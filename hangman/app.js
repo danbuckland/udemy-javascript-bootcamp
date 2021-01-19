4d8113b6f0ac5381d1130ad81ba6eb085bc5b964
Dan Buckland <danbucklan@gmail.com>
2021-01-19 13:20:21 +0000

    Implement http request for dynamic hangman game

--- hangman/app.js
+++ hangman/app.js
@@ -1,18 +1,25 @@
-
-
 const game1 = new Hangman('California', 5)
 const game2 = new Hangman('steady', 4)
 const game3 = new Hangman('High Score', 3)
 const puzzleEl = document.querySelector('#game')
 const guessesEl = document.querySelector('#guesses')
 
-puzzleEl.textContent = game3.puzzle
-guessesEl.textContent = `Guesses remaining: ${game3.remainingGuesses}`
 
-console.log(game3.puzzle)
-
-window.addEventListener('keypress', (e) => {
-  game3.guess(e.key)
-  puzzleEl.textContent = game3.puzzle
-  guessesEl.textContent = game3.statusMessage
+// Making an http request
+const request = new XMLHttpRequest()
+request.addEventListener('readystatechange', (e) => {
+  if (e.target.readyState === 4) {
+    const data = JSON.parse(e.target.responseText)
+    const dynamicGame = new Hangman(data.puzzle, 10)
+    puzzleEl.textContent = dynamicGame.puzzle
+    guessesEl.textContent = `Guesses remaining: ${dynamicGame.remainingGuesses}`
+    window.addEventListener('keypress', (e) => {
+      dynamicGame.guess(e.key)
+      puzzleEl.textContent = dynamicGame.puzzle
+      guessesEl.textContent = dynamicGame.statusMessage
+    })
+  }
 })
+
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()
