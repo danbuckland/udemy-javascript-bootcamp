813030a25b6197df89e8b0174152b3bd34b285dc
Dan Buckland <danbucklan@gmail.com>
2021-01-19 16:08:42 +0000

    Create a callback function to getPuzzle

--- hangman/app.js
+++ hangman/app.js
@@ -3,41 +3,35 @@ const game2 = new Hangman('steady', 4)
 const game3 = new Hangman('High Score', 3)
 const puzzleEl = document.querySelector('#game')
 const guessesEl = document.querySelector('#guesses')
+const dynamicGame = new Hangman('DEFAULTGAME', 5)
 
-
-// Making an http request
-const request = new XMLHttpRequest()
-request.addEventListener('readystatechange', (e) => {
-  if (e.target.readyState === 4 && e.target.status === 200) {
-    const data = JSON.parse(e.target.responseText)
-    const dynamicGame = new Hangman(data.puzzle, 5)
+getPuzzle((error, puzzle) => {
+  if (error) {
+    console.log(`Error: ${error}`)
+  }
+  const dynamicGame = new Hangman(puzzle, 5)
+  puzzleEl.textContent = dynamicGame.puzzle
+  guessesEl.textContent = `Guesses remaining: ${dynamicGame.remainingGuesses}`
+  
+  window.addEventListener('keypress', (e) => {
+    dynamicGame.guess(e.key)
     puzzleEl.textContent = dynamicGame.puzzle
-    guessesEl.textContent = `Guesses remaining: ${dynamicGame.remainingGuesses}`
-
-    window.addEventListener('keypress', (e) => {
-      dynamicGame.guess(e.key)
-      puzzleEl.textContent = dynamicGame.puzzle
-      guessesEl.textContent = dynamicGame.statusMessage
-    })
-  } else if (e.target.readyState === 4) {
-    console.log('An error has occurred')
-  }
+    guessesEl.textContent = dynamicGame.statusMessage
+  })
 })
 
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
-request.send()
 
-const coutriesReq = new XMLHttpRequest()
+// const coutriesReq = new XMLHttpRequest()
 
-coutriesReq.addEventListener('readystatechange', (e) => {
-  if (e.target.readyState === 4 && e.target.status === 200) {
-    const countriesData = JSON.parse(e.target.responseText)
-    const myCountry = countriesData.find(country => country.alpha2Code === 'GB')
-    console.log(myCountry.name)
-  } else if (e.target.readyState === 4) {
-    console.log('An error has occurred')
-  }
-})
+// coutriesReq.addEventListener('readystatechange', (e) => {
+//   if (e.target.readyState === 4 && e.target.status === 200) {
+//     const countriesData = JSON.parse(e.target.responseText)
+//     const myCountry = countriesData.find(country => country.alpha2Code === 'GB')
+//     console.log(myCountry.name)
+//   } else if (e.target.readyState === 4) {
+//     console.log('An error has occurred')
+//   }
+// })
 
-coutriesReq.open('GET', 'http://restcountries.eu/rest/v2/all')
-coutriesReq.send()
+// coutriesReq.open('GET', 'http://restcountries.eu/rest/v2/all')
+// coutriesReq.send()
