852b647eb1d46126aae1477c5581a9516a1fda0d
Dan Buckland <danbucklan@gmail.com>
2021-01-20 13:37:27 +0000

    Learn how to use closures

---
+++ functions/closures.js
@@ -0,0 +1,46 @@
+const createCounter = () => {
+  let count = 0
+
+  return {
+    increment() {
+      count++
+    },
+    decrement() {
+      count--
+    },
+    get() {
+      return count
+    }
+  }
+}
+
+const counter = createCounter()
+counter.increment()
+counter.decrement()
+counter.decrement()
+console.log(counter.get())
+
+// Adder
+const createAdder = (a) => {
+  return (b) => {
+    return a + b
+  }
+} 
+
+const add10 = createAdder(10)
+console.log(add10(-2))
+console.log(add10(20))
+const add100 = createAdder(100)
+console.log(add100(-90))
+
+// Tipper
+const createTipper = (percent) => {
+  return (bill) => {
+    return bill + (bill * percent / 100)
+  }
+}
+
+const averageTip = createTipper(15)
+const generousTip = createTipper(20)
+console.log(averageTip(100)) // 115
+console.log(generousTip(110)) // 132
